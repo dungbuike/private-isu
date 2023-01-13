b124255675d2843075c70e726e23a0973b969144
dungbuike <dung.buike@prtimes.co.jp>
2023-01-13 16:28:02 +0900

    write image to file

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -807,7 +807,7 @@ func generateImage(w http.ResponseWriter, r *http.Request) {
 	_ = db.Select(&results, "SELECT * FROM `posts`")
 
 	for _, p := range results {
-		imagePath := "../public" + imageURL(p)
+		imagePath := "/home/isucon/private_isu/webapp/public" + imageURL(p)
 		f, err := os.Create(imagePath)
 		if err != nil {
 			fmt.Println(err)
